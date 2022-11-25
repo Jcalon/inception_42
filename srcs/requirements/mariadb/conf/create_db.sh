2b93380cfc2af125ccf9cd1e8be22c23af7f3ff2
jcalon <juliencalon@hotmail.fr>
2022-11-25 10:11:41 -0500

    AllBroken

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,39 +1,38 @@
-#!bin/sh
+#!/bin/sh
 
-#Check si MySQL est en cours d'exec et si non on le demarre (normalement ce bloc est ignore)
-if [ ! -d "/var/lib/mysql/mysql" ]; then
+if [ ! -d /var/lib/mysql/wordpress ]
+then
 
-        chown -R mysql:mysql /var/lib/mysql
+	echo "Starting temporary server..."
+	mariadbd --user=root &
 
-        #Init de la DB
-        mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm
+	sleep 3
 
-        tfile=`mktemp`
-        if [ ! -f "$tfile" ]; then
-                return 1
-        fi
+	echo "Creating Wordpress database..."
+	mysql --user=root <<- _EOF_
+		CREATE DATABASE $DB_NAME;
+		CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
+		GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';
+		FLUSH PRIVILEGES;
+	_EOF_
+	echo "Wordpress database created!"
+
+	echo "Securing the MYSQL installation..."
+	mysql --user=root <<- _EOF_
+		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
+		DELETE FROM mysql.user WHERE User='';
+		DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+		DROP DATABASE IF EXISTS test;
+		DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
+		FLUSH PRIVILEGES;
+	_EOF_
+	echo "MYSQL installation secured!"
+
+	echo "Stopping temporary server!"
+	mysqladmin --user=root --password=$DB_ROOT shutdown
+
+	sleep 3
 fi
 
-#Check si une DB WP existe
-if [ ! -d "/var/lib/mysql/wordpress" ]; then
-
-#En Structured Query Language, creation de la DB avec le .env
-        cat << EOF > /tmp/create_db.sql
-USE mysql;
-FLUSH PRIVILEGES;
-DELETE FROM     mysql.user WHERE User='';
-DROP DATABASE test;
-DELETE FROM mysql.db WHERE Db='test';
-DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE ${DB_NAME} CHARACTER SET utf8 COLLATE utf8_general_ci;
-CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
-FLUSH PRIVILEGES;
-EOF
-
-#Exec de init.sql et supp le fichier de config supplementaire pour pas laisser de traces
-          /usr/bin/mysqld --user=mysql --bootstrap < /tmp/create_db.sql
-          rm -f /tmp/create_db.sql
-
-fi
+echo "Starting mariadb server..."
+exec mariadbd --user=root
--- srcs/requirements/wordpress/conf/config_wp.sh
+++ srcs/requirements/wordpress/conf/config_wp.sh
@@ -1,42 +1,28 @@
-#!bin/sh
+#!/bin/sh
 
-#Configuration de WP et connection au conteneur de la DB
+while ! mysql --user=$DB_USER --password=$DB_PASS -h mariadb --execute "SHOW DATABASES;" > /dev/null
+do
+	sleep 5
+done
 
-if [ ! -f "/var/www/wp-config.php" ]; then
-cat << EOF > /var/www/wp-config.php
-<?php
-define( 'DB_NAME', '${DB_NAME}' );
-define( 'DB_USER', '${DB_USER}' );
-define( 'DB_PASSWORD', '${DB_PASS}' );
-define( 'DB_HOST', 'mariadb' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
-define('FS_METHOD','direct');
-\$table_prefix = 'wp_';
-define( 'WP_DEBUG', false );
+if [ ! -f /var/www/wp-config.php ]
+then
+	echo "Generating configuration file for Wordpress..."
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=mariadb
+	echo "Wordpress configuration file generated!"
 
-#Reglages liés a redis pour les bonus
-if ( ! defined( 'ABSPATH' ) ) {
-define( 'ABSPATH', __DIR__ . '/' );}
-define( 'WP_REDIS_HOST', 'redis' );
-define( 'WP_REDIS_PORT', 6379 );
-define( 'WP_REDIS_TIMEOUT', 1 );
-define( 'WP_REDIS_READ_TIMEOUT', 1 );
-define( 'WP_REDIS_DATABASE', 0 );
-require_once ABSPATH . 'wp-settings.php';
-EOF
+	echo "Installing Wordpress..."
+	wp core install --url=localhost --title="Inception" --admin_user=$WP_ADMIN --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
+	echo "Wordpress is installed!"
 
-        echo "Installing Wordpress..."
-        wp core install --url=jcalon.42.fr --title="Inception" --admin_user=$WP_ADMIN \
-                --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email
-        echo "Wordpress is installed!"
+	echo "Creating second user.."
+	wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author
+	echo "Second user created!"
 
-        echo "Creating second user.."
-        wp user create $WP_USER $WP_USER_EMAIL --user_pass=$WP_USER_PASS --role=author
-        echo "Second user created!"
-
-        echo "Installing Wordpress theme..."
-        wp theme install twentytwentyone --activate
-        echo "Wordpress theme installed!"
-
+	echo "Installing Wordpress theme..."
+	wp theme install twentytwentyone --activate
+	echo "Wordpress theme installed!"
 fi
+
+echo "Starting PHP-FPM..."
+exec php-fpm8 -F
