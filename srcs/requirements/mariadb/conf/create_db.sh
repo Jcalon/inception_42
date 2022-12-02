e5e0d07bdb043b5710e98385320abdd676f117cc
Julien CALON <jcalon@e3r5p5.clusters.42paris.fr>
2022-12-02 17:53:00 +0100

    Update

--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -25,9 +25,9 @@ DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${DB_ROOT}';
-CREATE DATABASE '${DB_NAME}' CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE wordpress CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '${DB_USER}'@'%' IDENTIFIED by '${DB_PASS}';
-GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO '${DB_USER}'@'%';
+GRANT ALL PRIVILEGES ON wordpress.* TO '${DB_USER}'@'%';
 FLUSH PRIVILEGES;
 EOF
 	#Exec de init.sql et supp le fichier de config supplementaire pour pas laisser de traces
