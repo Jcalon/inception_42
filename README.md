def29a73450758265c3f3965d3a21cca2fb931ad
Julien CALON <jcalon@e3r5p5.clusters.42paris.fr>
2022-12-02 13:51:34 +0100

    Update

---
+++ README.md
@@ -0,0 +1,3 @@
+docker exec -it wordpress php -v
+docker exec -it wordpress ps aux | grep 'php'
+docker exec -it wordpress php -m
--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -21,14 +21,17 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
 	cat << EOF > $tfile
 USE mysql;
 FLUSH PRIVILEGES;
+
 DELETE FROM	mysql.user WHERE User='';
 DROP DATABASE test;
 DELETE FROM mysql.db WHERE Db='test';
 DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
 CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
 CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PASS';
 GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+
 FLUSH PRIVILEGES;
 EOF
 	#Exec de init.sql et supp le fichier de config supplementaire pour pas laisser de traces
