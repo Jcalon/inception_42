9a6e7f4a4cf90f5eda62ce612a0e551db21dd25b
Julien CALON <jcalon@e3r5p5.clusters.42paris.fr>
2022-12-02 17:31:53 +0100

    Update

--- srcs/requirements/bonus/vsftpd/tools/config_ftp.sh
+++ srcs/requirements/bonus/vsftpd/tools/config_ftp.sh
@@ -9,11 +9,11 @@ if [ ! -f "/etc/vsftpd/vsftpd.conf.bak" ]; then
     mv /tmp/vsftpd.conf /etc/vsftpd/vsftpd.conf
 
 	# On joute l'user et on le rend proprio du WP
-    adduser $FTP_USR --disabled-password
-    echo "$FTP_USR:$FTP_PWD" | /usr/sbin/chpasswd &> /dev/null
-    chown -R $FTP_USR:$FTP_USR /var/www/html
+    adduser $FTP_USER --disabled-password
+    echo "$FTP_USER:$FTP_PASS" | /usr/sbin/chpasswd &> /dev/null
+    chown -R $FTP_USER:$FTP_USER /var/www/html
 
-    echo $FTP_USR | tee -a /etc/vsftpd.userlist &> /dev/null
+    echo $FTP_USER | tee -a /etc/vsftpd.userlist &> /dev/null
 
 fi
 
