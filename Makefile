b294eaee67b9d6b7540d235f3fc29b471ef691ba
Julien CALON <jcalon@e3r5p5.clusters.42paris.fr>
2022-12-02 13:13:10 +0100

    Update

--- Makefile
+++ Makefile
@@ -1,36 +1,20 @@
 NAME = inception
 
-all:
-	@echo -e "Launching configuration ${NAME}\n"
-	@bash srcs/requirements/wordpress/tools/makedirectory.sh
-	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d
+all: prune reload
 
-build:
-	@echo -e "Building configuration ${NAME}\n"
-	@bash srcs/requirements/wordpress/tools/makedirectory.sh
-	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d --build
+linux:
+	@ echo "127.0.0.1 raccoman.42.fr" >> /etc/hosts
+	
+stop:
+	@ docker-compose -f srcs/docker-compose.yml down
 
-down:
-	@echo -e "Stopping configuration ${NAME}\n"
-	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env down
+clean: stop
+	@ rm -rf ~/Desktop/inception
 
-re: down
-	@echo -e "Re Building configuration ${NAME}\n"
-	@docker-compose -f ./srcs/docker-compose.yml --env-file srcs/.env up -d --build
+prune: clean
+	@ docker system prune -f
 
-clean: down
-	@echo -e "Cleaning configuration ${NAME}\n"
-	@docker system prune -a
-	@sudo rm -rf ~/data/wordpress/*
-	@sudo rm -rf ~/data/mariadb/*
+reload: 
+	@ docker-compose -f srcs/docker-compose.yml up --build
 
-fclean:
-	@echo "Cleaning all configurations docker\n"
-	@docker stop $$(docker ps -qa)
-	@docker system prune --all --force --volumes
-	@docker network prune --force
-	@docker volume prune --force
-	@sudo rm -rf ~/data/wordpress/*
-	@sudo rm -rf ~/data/mariadb/*
-
-.PHONY: all build down re clean fclean
+.PHONY: linux stop clean prune reload all
--- srcs/requirements/mariadb/conf/create_db.sh
+++ srcs/requirements/mariadb/conf/create_db.sh
@@ -1,38 +1,40 @@
 #!/bin/sh
 
-if [ ! -d /var/lib/mysql/wordpress ]
-then
+if [ ! -d "/run/mysqld" ]; then
+	mkdir -p /run/mysqld
+	chown -R mysql:mysql /run/mysqld
+fi
 
-	echo "Starting temporary server..."
-	mariadbd --user=root &
+#Check si MySQL est en cours d'exec et si non on le demarre
+if [ ! -d "/var/lib/mysql/mysql" ]; then
+	
+	chown -R mysql:mysql /var/lib/mysql
 
-	sleep 3
+	#Creation de la base de donnee a partir de la config Dockerfile (on precise ou la DB va etre stockee)
+	mysql_install_db --basedir=/usr --datadir=/var/lib/mysql --user=mysql --rpm > /dev/null
 
-	echo "Creating Wordpress database..."
-	mysql --user=root <<- _EOF_
-		CREATE DATABASE $DB_NAME;
-		CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';
-		GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%';
-		FLUSH PRIVILEGES;
-	_EOF_
-	echo "Wordpress database created!"
-
-	echo "Securing the MYSQL installation..."
-	mysql --user=root <<- _EOF_
-		ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
-		DELETE FROM mysql.user WHERE User='';
-		DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
-		DROP DATABASE IF EXISTS test;
-		DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
-		FLUSH PRIVILEGES;
-	_EOF_
-	echo "MYSQL installation secured!"
-
-	echo "Stopping temporary server!"
-	mysqladmin --user=root --password=$DB_ROOT shutdown
-
-	sleep 3
+	tfile=`mktemp`
+	if [ ! -f "$tfile" ]; then
+		return 1
+	fi
+#En Structured Query Language, creation de la DB avec le .env
+	cat << EOF > $tfile
+USE mysql;
+FLUSH PRIVILEGES;
+DELETE FROM	mysql.user WHERE User='';
+DROP DATABASE test;
+DELETE FROM mysql.db WHERE Db='test';
+DELETE FROM mysql.user WHERE User='root' AND Host NOT IN ('localhost', '127.0.0.1', '::1');
+ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT';
+CREATE DATABASE $DB_NAME CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE USER '$DB_USER'@'%' IDENTIFIED by '$DB_PASS';
+GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';
+FLUSH PRIVILEGES;
+EOF
+	#Exec de init.sql et supp le fichier de config supplementaire pour pas laisser de traces
+	/usr/bin/mysqld --user=mysql --bootstrap < $tfile
+	rm -f $tfile
 fi
 
 echo "Starting mariadb server..."
-exec mariadbd --user=root
+exec /usr/bin/mysqld --user=mysql --console
